7d22957859179efc83ee8c7de3a64590f8e0203c
samuel-joly <samuel.joly@laplateforme.io>
2021-07-22 12:35:00 +0200

    fixxed query on error stat

--- app/Models/LogsModel.php
+++ app/Models/LogsModel.php
@@ -60,7 +60,7 @@ DATE_FORMAT(from_unixtime(date_of_request), '%M')")->getResult();
 
     public function countErrorLogs($service) {
         $this->table = $service;
-        $logs = $this->query("select count(request_status), DATE_FORMAT(from_unixtime(date_of_request), '%M') from $service where request_status >= 400 AND request_status < 500 AND request_url != 'GET /favicon.ico HTTP/1.1' AND request_url != 'GET / HTTP/1.1' group by DATE_FORMAT(from_unixtime(date_of_request), '%M')")->getResult();
+        $logs = $this->query("select count(hostname), DATE_FORMAT(from_unixtime(date_of_request), '%M') from $service where request_status >= 400 AND request_status < 500 AND request_url != 'GET /favicon.ico HTTP/1.1' AND request_url != 'GET / HTTP/1.1' group by DATE_FORMAT(from_unixtime(date_of_request), '%M')")->getResult();
         return $logs;
     }
 
